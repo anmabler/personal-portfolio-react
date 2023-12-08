ad1d777f2df9e82ca19b06b2d65190f6b5782a2b
Martina <a.martina.eriksson@hotmail.com>
2023-12-08 15:53:52 +0100

    feat: adds navlinks

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-        Nav
+    <div className="sidebar fixed top-0 bottom-0 w-3/12 bg-slate-500">
+        <ul className='flex flex-col gap-y-20 mt-6'>
+          <NavLink className='p-5' to="/">Home</NavLink>
+          <NavLink className='p-5' to="/about">About me</NavLink>
+          <NavLink className='p-5' to="/experience">Experience</NavLink>
+          <NavLink className='p-5' to="/contact">Contact</NavLink>
+        </ul>
     </div>
   )
 }
