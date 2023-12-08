4e801c9d4259dba42ac41362b7eec1d73f52a706
Martina <a.martina.eriksson@hotmail.com>
2023-12-08 10:12:10 +0100

    feat: adds tailwind css

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+      Home
+    </div>
   )
 }
