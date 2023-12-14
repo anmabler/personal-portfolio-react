d5e85a6f0ad75f74a7ca58a7275826aebbd52b66
Martina <a.martina.eriksson@hotmail.com>
2023-12-14 14:00:37 +0100

    fix: formats code and adds css

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function HomeContent() {
   return (
-    <div className='p-10 flex'>
-        <div className=''>
-        <img src="https://thispersondoesnotexist.com/" alt="profile" className='object-cover' />
+    <>
+      <h1 className="font-bold text-5xl my-4">Lorem ipsum dolor sit.</h1>
+      <div className="p-10 flex justify-center items-baseline h-full">
+        <div className="">
+          <img
+            src="https://thispersondoesnotexist.com/"
+            alt="profile"
+            className="object-cover rounded-full"
+          />
         </div>
-            
-        <div className=''>
-            <h1 className='font-bold text-5xl my-4'>Lorem ipsum dolor sit.</h1>
-            <p className='p-5'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum est odit animi ipsa odio quas itaque accusamus, quia atque impedit. Aliquid, a facere doloremque doloribus minima voluptatum aspernatur corrupti nisi cupiditate? Ducimus animi temporibus voluptates autem nemo optio atque unde assumenda recusandae provident aliquam doloremque nisi, necessitatibus quo, similique architecto!
-            </p>
+
+        <div>
+          <p className="p-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            est odit animi ipsa odio quas itaque accusamus, quia atque impedit.
+            Aliquid, a facere doloremque doloribus minima voluptatum aspernatur
+            corrupti nisi cupiditate? Ducimus animi temporibus voluptates autem
+            nemo optio atque unde assumenda recusandae provident aliquam
+            doloremque nisi, necessitatibus quo, similique architecto!
+          </p>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
