d99373ee11a1967ff252c8f8e7d2ce86ec353a4f
Martina <a.martina.eriksson@hotmail.com>
2023-12-14 11:15:04 +0100

    feat: dummy img and text on home page

---
+++ src/components/HomeContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function HomeContent() {
+  return (
+    <div className='p-10 flex'>
+        <div className=''>
+        <img src="https://thispersondoesnotexist.com/" alt="profile" className='object-cover' />
+        </div>
+            
+        <div className=''>
+            <h1 className='font-bold text-5xl my-4'>Lorem ipsum dolor sit.</h1>
+            <p className='p-5'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum est odit animi ipsa odio quas itaque accusamus, quia atque impedit. Aliquid, a facere doloremque doloribus minima voluptatum aspernatur corrupti nisi cupiditate? Ducimus animi temporibus voluptates autem nemo optio atque unde assumenda recusandae provident aliquam doloremque nisi, necessitatibus quo, similique architecto!
+            </p>
+        </div>
+    </div>
+  )
+}
