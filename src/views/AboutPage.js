5c6cc0c0f4b0365e90a126a8f3c2748cbaf11d7e
Martina <a.martina.eriksson@hotmail.com>
2023-12-15 09:26:55 +0100

    feat: about page content

--- src/views/AboutPage.js
+++ src/views/AboutPage.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function AboutPage() {
   return (
-    <div>About</div>
+    <div>About
+      <div className='m-20'>
+        <h3 className='font-bold text-left'>Something about my work experience</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dicta omnis necessitatibus autem vel nostrum, nisi delectus veritatis consectetur dolore quam saepe rerum adipisci ipsa doloremque iusto aliquid atque explicabo eos consequuntur temporibus! Enim, amet blanditiis! Iusto voluptas magnam repudiandae qui dignissimos, eum quisquam ea eaque accusantium rem tenetur corrupti consequuntur voluptate sunt quidem temporibus. Cum eligendi sint corrupti incidunt omnis voluptatum assumenda mollitia aperiam excepturi ipsa, quam odit quis optio in dolorum reprehenderit vitae velit necessitatibus aliquam iure, non nulla sit nemo. Vero, illo. Reiciendis in obcaecati excepturi quam praesentium beatae neque. Impedit error nam asperiores adipisci perferendis cum.</p>
+      </div>
+      <div className='m-20'>
+        <h3 className='font-bold text-left'>Something about my education</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dicta omnis necessitatibus autem vel nostrum, nisi delectus veritatis consectetur dolore quam saepe rerum adipisci ipsa doloremque iusto aliquid atque explicabo eos consequuntur temporibus! Enim, amet blanditiis! Iusto voluptas magnam repudiandae qui dignissimos, eum quisquam ea eaque accusantium rem tenetur corrupti consequuntur voluptate sunt quidem temporibus. Cum eligendi sint corrupti incidunt omnis voluptatum assumenda mollitia aperiam excepturi ipsa, quam odit quis optio in dolorum reprehenderit vitae velit necessitatibus aliquam iure, non nulla sit nemo. Vero, illo. Reiciendis in obcaecati excepturi quam praesentium beatae neque. Impedit error nam asperiores adipisci perferendis cum.</p>
+      </div>
+      <div className='m-20'>
+        <h3 className='font-bold text-left'>Maybe something personal</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et fugit rem nemo reiciendis odit quos, officiis inventore, incidunt recusandae, quae explicabo. Ipsam asperiores, laborum debitis ipsum ex earum eligendi! Voluptas quidem repudiandae reiciendis maiores quas, quaerat dolorem repellat, consequatur saepe labore commodi provident. Deserunt, consequatur aliquam. Reiciendis odit quis sit hic saepe. Eveniet quo sit harum dolorem laboriosam recusandae tempore qui, est, officiis similique, enim consequuntur. Dolorum ex dignissimos assumenda.</p>
+      </div>
+
+    </div>
   )
 }
