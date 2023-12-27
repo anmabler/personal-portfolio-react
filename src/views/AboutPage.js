f13a61231e5ff3450ae00f52cf398b7fd3a2f40d
Martina <a.martina.eriksson@hotmail.com>
2023-12-27 15:30:41 +0100

    feat: adds info to about page

--- src/views/AboutPage.js
+++ src/views/AboutPage.js
@@ -2,17 +2,36 @@ import React from 'react'
 
 export default function AboutPage() {
   return (
-    <div className='max-w-xl m-auto'>About
+    <div className='max-w-xl m-auto'>
       <div className='m-20 text-left'>
-        <h3 className='font-bold'>Something about my work experience</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dicta omnis necessitatibus autem vel nostrum, nisi delectus veritatis consectetur dolore quam saepe rerum adipisci ipsa doloremque iusto aliquid atque explicabo eos consequuntur temporibus! Enim, amet blanditiis! Iusto voluptas magnam repudiandae qui dignissimos, eum quisquam ea eaque accusantium rem tenetur corrupti consequuntur voluptate sunt quidem temporibus. Cum eligendi sint corrupti incidunt omnis voluptatum assumenda mollitia aperiam excepturi ipsa, quam odit quis optio in dolorum reprehenderit vitae velit necessitatibus aliquam iure, non nulla sit nemo. Vero, illo. Reiciendis in obcaecati excepturi quam praesentium beatae neque. Impedit error nam asperiores adipisci perferendis cum.</p>
+        <h3 className='font-bold text-lg mb-4'>Work experience</h3>
+        <div className='mb-5'>  
+          <h4 className='font-bold'>Fullstack developer, Experis Academy | July 2022 – July 2023</h4>
+          <p>
+            Experis Academy is an intensive training program where you learn about technologies like Java 13, Spring, Hibernate, REST, Apache
+            Tomcat, SOAP, NodeJS, Javascript 101, ReactJS, Angular, Bootstrap 4, HTML5, CSS, and Scrum during a 3-month period. Afterwards,
+            I had the opportunity to further develop my skills in Java through various personal projects and online courses
+          </p>
+        </div>
       </div>
       <div className='m-20 text-left'>
-        <h3 className='font-bold'>Something about my education</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dicta omnis necessitatibus autem vel nostrum, nisi delectus veritatis consectetur dolore quam saepe rerum adipisci ipsa doloremque iusto aliquid atque explicabo eos consequuntur temporibus! Enim, amet blanditiis! Iusto voluptas magnam repudiandae qui dignissimos, eum quisquam ea eaque accusantium rem tenetur corrupti consequuntur voluptate sunt quidem temporibus. Cum eligendi sint corrupti incidunt omnis voluptatum assumenda mollitia aperiam excepturi ipsa, quam odit quis optio in dolorum reprehenderit vitae velit necessitatibus aliquam iure, non nulla sit nemo. Vero, illo. Reiciendis in obcaecati excepturi quam praesentium beatae neque. Impedit error nam asperiores adipisci perferendis cum.</p>
+        <h3 className='font-bold text-lg mb-4'>Education</h3>
+        <div className='mb-5'>
+          <h4 className='font-bold'>Web development with e-commerce focus, Grit Academy | 2022</h4>
+          <p>In the Entrepreneurship and Consultancy course, I learned how to go from an idea to a product, created a business plan with
+          accompanying needs analysis, and improved my presentation skills. In the Design and UX course, the emphasis was on making
+          websites user-friendly. I also took courses in HTML, CSS, and JavaScript during the semester.</p>
+        </div>
+        <div className='mb-5'>
+          <h4 className='font-bold'>Object oriented programming with AI competence, Yrkeshögskoleexamen, Handelsakademin | 2020 - 2021</h4>
+          <p>In this program, I studied Java, JavaScript, Python, HTML, and CSS and gained experience in AI. The program focused on the practical
+          application of these languages and technologies. I also gained experience working in teams, as most assignments were group projects.
+          I had the opportunity to work using SCRUM. In addition to my studies, I served as a student representative, facilitating class meetings
+          and presenting the class's opinions at leadership meetings.</p>
+        </div>
       </div>
       <div className='m-20 text-left'>
-        <h3 className='font-bold'>Maybe something personal</h3>
+        <h3 className='font-bold text-lg mb-4'>Personal</h3>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et fugit rem nemo reiciendis odit quos, officiis inventore, incidunt recusandae, quae explicabo. Ipsam asperiores, laborum debitis ipsum ex earum eligendi! Voluptas quidem repudiandae reiciendis maiores quas, quaerat dolorem repellat, consequatur saepe labore commodi provident. Deserunt, consequatur aliquam. Reiciendis odit quis sit hic saepe. Eveniet quo sit harum dolorem laboriosam recusandae tempore qui, est, officiis similique, enim consequuntur. Dolorum ex dignissimos assumenda.</p>
       </div>
 
