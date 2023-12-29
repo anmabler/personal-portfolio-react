1ef44e95ca9ab974b369e337d4b2d494e33f02c5
Martina <a.martina.eriksson@hotmail.com>
2023-12-29 10:14:45 +0100

    fix: border radius on hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ title, description, imageUrl }) => {
     return (
-      <div className="bg-orchid-pink rounded-xl overflow-hidden shadow-lg hover:scale-105  border-orchid-pink hover:border-8">
+      <div className="bg-orchid-pink rounded-xl overflow-hidden shadow-lg hover:scale-105  border-orchid-pink hover:rounded-md hover:border-8">
         {imageUrl && <img className="" src={imageUrl} alt={title} />}
         <div className="px-6 py-4 text-start">
           <div className="font-bold text-xl mb-2">{title}</div>
