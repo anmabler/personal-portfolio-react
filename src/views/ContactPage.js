afdc9aa3dd05cccf8b7dddddb9e711e9000237f6
Martina <a.martina.eriksson@hotmail.com>
2024-02-09 11:25:11 +0100

    feat: social media logos

--- src/views/ContactPage.js
+++ src/views/ContactPage.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import linkedin from '../images/LI-In-Bug.png'
+import github from '../images/github-mark.png'
 
 export default function ContactPage() {
   return (
-    <div>Contact</div>
+    <div className='flex mx-auto'>
+      <div className='pt-4 w-3/12 m-2'><a href="https://www.linkedin.com/in/martina-blixt-eriksson/"><img src={linkedin} alt="LinkedIn" /></a></div>
+      <div className="w-3/12 m-2 "><a href="https://github.com/anmabler"><img src={github} alt="GitHub" /></a></div>
+      <div></div>
+    </div>
   )
 }
